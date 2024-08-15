8ea9ae655c621b4a776d43f3901e8b13c37562d4
aldiwildan77 <aldiwild77@gmail.com>
2024-08-15 09:50:56 +0700

    feat: add ui on history screen

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,13 +1,60 @@
-import { TopNavigation } from "@ui-kitten/components";
+import {
+  Button,
+  Icon,
+  IconElement,
+  IconProps,
+  List,
+  ListItem,
+  TopNavigation,
+} from "@ui-kitten/components";
 import { Fragment } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+interface IListHistory {
+  title: string;
+  description: string;
+}
+
+const data = new Array(8).fill({
+  title: "Your OCD Test Result",
+  description: "TODO: Tanggal Here",
+});
 
 export default function HistoryTabScreen() {
+  // TODO: bind data from firestore
+
+  const renderItemAccessory = (): React.ReactElement => (
+    <Button size="tiny">Check</Button>
+  );
+
+  const renderItemIcon = (props: IconProps): IconElement => (
+    <Icon {...props} name="person" />
+  );
+
+  const renderItem = ({
+    item,
+    index,
+  }: {
+    item: IListHistory;
+    index: number;
+  }): React.ReactElement => (
+    <ListItem
+      title={`${item.title} ${index + 1}`}
+      description={`${item.description} ${index + 1}`}
+      accessoryLeft={renderItemIcon}
+      accessoryRight={renderItemAccessory}
+    />
+  );
+
   return (
     <Fragment>
       <TopNavigation title="History" />
       <View style={styles.container}>
-        <Text>History page</Text>
+        <List
+          style={styles.listContainer}
+          data={data}
+          renderItem={renderItem}
+        />
       </View>
     </Fragment>
   );
@@ -18,5 +65,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     padding: 16,
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
   },
 });
