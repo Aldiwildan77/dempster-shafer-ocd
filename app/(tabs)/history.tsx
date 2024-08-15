c5c697b399139f87f048f5ea519ed0cb838dedef
aldiwildan77 <aldiwild77@gmail.com>
2024-08-15 09:23:03 +0700

    feat: add home info card

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import { TopNavigation } from "@ui-kitten/components";
+import { Fragment } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function History() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>History page</Text>
-    </View>
+    <Fragment>
+      <TopNavigation title="History" />
+      <View style={styles.container}>
+        <Text>History page</Text>
+      </View>
+    </Fragment>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,53 @@
-import { Text, View } from "react-native";
+import { Button, Card, Text, TopNavigation } from "@ui-kitten/components";
+import { Fragment } from "react";
+import { StyleSheet, View } from "react-native";
 
 export default function Index() {
+  const TakeTestHeader = () => {
+    return (
+      <View style={{ gap: 8 }}>
+        <Text category="h4">Take a quick OCD Test</Text>
+        <Text style={{ textAlign: "justify" }}>
+          This test may help identify if you are suffering with OCD and to what
+          degree. It is not meant to replace a full assessment by a qualified
+          clinician and we would always recommend going to a qualified,
+          experienced clinician if you have any concerns about your results or
+          your health.
+        </Text>
+      </View>
+    );
+  };
+
+  const TakeTestFooter = () => {
+    return (
+      <Button
+        onPress={() => {
+          // TODO: Navigate to the test page
+        }}
+      >
+        Start Test
+      </Button>
+    );
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Index page</Text>
-    </View>
+    <Fragment>
+      <TopNavigation title="OCD Test" />
+      <View style={styles.container}>
+        <Card
+          header={TakeTestHeader}
+          footer={TakeTestFooter}
+          style={{ padding: 16 }}
+        ></Card>
+      </View>
+    </Fragment>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+});
