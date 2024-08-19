7d7790c47615427de845e65a2a0b32d59f1e9111
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2024-08-19 13:50:26 +0700

    chore: move file from component to page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,68 @@
-import { OCDHomeScreen } from "@/components/ocd/OcdHome";
 import { SafeAreaView } from "@/components/SafeAreaView";
 import { router } from "expo-router";
+import { Image, StyleSheet, View } from "react-native";
+import { Button, Text } from "@ui-kitten/components";
 
 export default function IndexTabScreen() {
   return (
     <SafeAreaView>
-      <OCDHomeScreen
-        onStart={() => {
-          router.push("/ocd/test");
-        }}
-      />
+      <View style={styles.container}>
+        <View style={styles.body}>
+          <Image
+            source={require("../../assets/images/trophy.png")}
+            style={{
+              width: 200,
+              height: 200,
+              marginHorizontal: "auto",
+            }}
+          />
+          <Text
+            category="h5"
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Buktikan Bahwa Kamu Bebas OCD
+          </Text>
+          <Text
+            category="h6"
+            style={{
+              textAlign: "center",
+              fontWeight: "normal",
+            }}
+          >
+            Ikuti tes sederhana ini untuk mengetahui apakah kamu memiliki gejala
+            OCD
+          </Text>
+        </View>
+        <View style={styles.footer}>
+          <Button
+            onPress={() => router.push("/ocd/test")}
+            style={{
+              width: "100%",
+            }}
+          >
+            Mulai Sekarang
+          </Button>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  body: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  footer: {
+    width: "100%",
+  },
+});
